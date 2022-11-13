d10afc4200c917e6a9246bedd523735d32027947
Ricardo Vera <ricardovera71@gmail.com>
2022-11-13 17:24:34 -0400

    finished project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import { useState, useEffect } from "react";
-import { Container, NextButton, QuestionContainer } from "./styles/App";
+import {
+  Answer,
+  AnswerContainer,
+  Container,
+  NextButton,
+  Question,
+  QuestionContainer,
+  QuestionTitle,
+} from "./styles/App.styles.js";
 import ReactConfetti from "react-confetti";
 import data from "./assets/PREGUNTAS EXPOZULIA.json";
+import headerImg from "./assets/HEADER_IMG.png"
+import footerImg from "./assets/FOOTER_IMG.png"
 
 const App = () => {
   const [windowDimensions, setWindowDimensions] = useState({
@@ -12,12 +22,14 @@ const App = () => {
   const [next, setNext] = useState(Math.floor(Math.random() * 39));
   const [score, setScore] = useState(0);
   const [currentQuestion, setCurrentCuestion] = useState(0);
+  const [selectedAns, setSelectedAns] = useState(null);
   const [isFinished, setIsFinished] = useState(false);
+  const [styledAns, setStyledAns] = useState("");
 
   useEffect(() => {
     window.addEventListener("resize", detectSize);
     return () => {
-      window.removeEventListener("resize",detectSize)
+      window.removeEventListener("resize", detectSize);
     };
   }, [windowDimensions]);
 
@@ -28,14 +40,17 @@ const App = () => {
     });
   };
 
-  const selectHandler = (isCorrect, e) => {
+  const selectHandler = (ans, e) => {
+    setSelectedAns(ans);
     setCurrentCuestion((currQuestion) => currQuestion + 1);
-    if (isCorrect) setScore((currScore) => currScore + 1);
-    e.target.className = isCorrect ? "correct" : "incorrect";
+    if (ans.isCorrect) setScore((currScore) => currScore + 1);
+    setStyledAns(ans.isCorrect ? "correct" : "incorrect");
+
     setTimeout(() => {
-      e.target.classList.remove(isCorrect ? "correct" : "incorrect");
+      setStyledAns("");
       if (currentQuestion === 2) {
         setIsFinished(true);
+        setNext(Math.floor(Math.random() * 39));
       } else {
         if (Math.floor(Math.random() * 39 === next)) setNext(next + 1);
         setNext(Math.floor(Math.random() * 39));
@@ -51,32 +66,42 @@ const App = () => {
 
   return (
     <Container>
-      <h2>TRIVIA Oz. 😎</h2>
+      <img alt="header" src={headerImg} width="300px"/>
       {!isFinished ? (
         <>
           <QuestionContainer>
-            <h4>{data[next]?.PREGUNTA}</h4>
-            <div>
+            <Question className="ribbon-outset border">
+              <QuestionTitle>PREGUNTA OZ:</QuestionTitle>
+              <h4>{data[next]?.PREGUNTA}</h4>
+            </Question>
+            <AnswerContainer>
               {data[next]?.OPCIONES.map((opt) => (
-                <button
+                <Answer
                   key={opt.key}
-                  onClick={(e) => selectHandler(opt.isCorrect, e)}
+                  onClick={(e) => selectHandler(opt, e)}
+                  className={`ribbon-outset border ${opt.key === selectedAns?.key && styledAns}`}
                 >
-                  <div>
-                    {opt.key} {")"} {opt.value}
-                  </div>
-                </button>
+                  {opt.key}- {opt.value}
+                </Answer>
               ))}
-            </div>
+            </AnswerContainer>
           </QuestionContainer>
         </>
       ) : (
         <div>
-          {score}
-          {score > 2 && <ReactConfetti width={windowDimensions.width} height={windowDimensions.height} tweenDuration={1000}/>}
-          <NextButton onClick={resetHandler}>Reiniciar</NextButton>
+          {score >= 2 && isFinished ? "GANASTE! 😄 🎉" : "INTENTA DE NUEVO 😕"}
+          <ReactConfetti
+            width={windowDimensions.width}
+            height={windowDimensions.height}
+            run={score >= 2 && isFinished}
+            tweenDuration={1000}
+          />
+          <NextButton className="ribbon-outset border" onClick={resetHandler}>Reiniciar</NextButton>
         </div>
       )}
+      <footer>
+        <img src={footerImg} alt="footer"  width="150px"/>
+      </footer>
     </Container>
   );
 };
